43909cb3481d06d4deff3ab8e1f68d454a991334
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-09-06 09:33:16 +0900

    Initial state

--- src/main/scala/com/suruguhamazaki/Boot.scala
+++ src/main/scala/com/suruguhamazaki/Boot.scala
@@ -1,14 +1,12 @@
 package com.suruguhamazaki
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor._
 import akka.io.IO
 import spray.can.Http
+import spray.http._
+import spray.routing._
 
 object Boot extends App {
 
-  implicit val system = ActorSystem("on-spray-can")
 
-  val service = system.actorOf(Props[MyServiceActor], "demo-service")
-
-  IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
 }
