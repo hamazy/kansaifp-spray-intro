a29001bb1cd1f032047721fe7f70fb7013b3b303
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-09-06 09:34:45 +0900

    Show how actors interact to serve an HTTP response to a request

--- src/main/scala/com/suruguhamazaki/Boot.scala
+++ src/main/scala/com/suruguhamazaki/Boot.scala
@@ -8,5 +8,23 @@ import spray.routing._
 
 object Boot extends App {
 
+  implicit val system = ActorSystem("on-spray-can")
+
+  system.actorOf(Props(new Actor with ActorLogging {
+
+    IO(Http) ! Http.Bind(self, interface = "localhost", port = 8080)
+
+    def receive: Receive = {
+      case _: Http.Bound ⇒ log.info(s"${self.path.name} started.")
+      case Http.Connected(remote, local) ⇒
+        log.info(s"Connected from $remote to $local")
+        sender ! Http.Register(self)
+      case req: HttpRequest ⇒
+        log.info(s"Request received: $req")
+        sender ! HttpResponse(entity = "Hello")
+      case _: Http.ConnectionClosed ⇒
+        log.info("Connetcion closed.")
+    }
+  }))
 
 }
